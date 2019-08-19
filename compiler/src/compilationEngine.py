5101b9b79a3ab657e1f90a22ab9c72e309911e66
PengFei_Gao <v_pega@163.com>
2019-08-19 18:28:40 +0800

    update

--- compiler/src/compilationEngine.py
+++ compiler/src/compilationEngine.py
@@ -1,4 +1,4 @@
-from tokenizer import tokenizer,transfer_XML
+from tokenizer import tokenizer,transfer_XML,KEYWORDS,SYMBOLS
 import pdb
 
 class CompilationEngine:
@@ -16,49 +16,48 @@ class CompilationEngine:
     'class' className '{' classVarDec* subroutineDec* '}'
     '''
     def compileClass(self):
-        curIdx = self.curIdx
         tokens = self.tokens
         XMLArr = self.XMLArr
 
         XMLArr += ['<class>\n']
         
         #verify token is 'class' and type is keyword
-        token,_type = self.getAndVerify(curIdx,e_token = 'class', e_type = 'keyword')
-        XMLArr += [transfer_XML(token, _type)]
-        
-        curIdx += 1
+        self.consume('class', 'keyword')
 
-        #verify type is identifier
-        token,_type = self.getAndVerify(curIdx, e_type = 'identifier')
-        XMLArr += [transfer_XML(token, _type)]
+        self.consume(e_type = 'identifier')
 
-        curIdx += 1
+        self.consume('{', 'symbol')
 
-        #verify token is '{' and type is symbol
-        token,_type = self.getAndVerify(curIdx,'{', 'symbol')
-        XMLArr += [transfer_XML(token, _type)]
-
-        curIdx += 1
-        self.curIdx = curIdx
-
-        #while tokens[self.curIdx][0] == 'field' or tokens[self.curIdx][0] == 'static':
-            #self.compileClassVarDec()            
-
-
+        while tokens[self.curIdx][0] == 'field' or tokens[self.curIdx][0] == 'static':
+            self.compileClassVarDec()            
 
         XMLArr += ['</class>\n']
     
-        print(XMLArr)
+        self.printXML()
         
         pass
 
+
+    '''    
+    ('static'|'field')type varName(','varName)*';'
+    '''
     def compileClassVarDec(self):
-        curIdx = self.curIdx
         tokens = self.tokens
         XMLArr = self.XMLArr
 
         XMLArr += ['<classVarDec>\n']
+        
+        self.consume(['static','field'],'keyword')
 
+        self.consume()
+
+        self.consume(e_type = 'identifier')
+        
+        while tokens[self.curIdx][0] != ';':
+            self.consume(',','symbol')
+            self.consume(e_type = 'identifier')
+
+        self.consume(';','symbol')
         XMLArr += ['</classVarDec>\n']
 
     def compileSubroutineDec(self):
@@ -94,9 +93,24 @@ class CompilationEngine:
     def getAndVerify(self, idx, e_token = None, e_type = None):
         tokens = self.tokens
         token,_type = tokens[idx][0], tokens[idx][1]
-        if e_token: assert token == e_token
-        if e_type: assert _type == e_type
+        if e_token: 
+            if type(e_token) is str: assert token == e_token
+            elif type(e_token) is list:
+                if not token in e_token: raise Exception(f"token {token} not in {e_token}")
+        if e_type and type:
+            if type(e_type) is str: assert _type == e_type
+            elif type(e_type) is list:
+                if not _type in e_type: raise Exception(f"_type {_type} not in {e_type}")
         return token,_type
+    
+    def consume(self, e_token=None, e_type=None):
+        token,_type = self.getAndVerify(self.curIdx, e_token, e_type)
+        self.XMLArr += [transfer_XML(token, _type)]
+        self.curIdx +=1
+
+    def printXML(self):
+        for tag in self.XMLArr:
+            print(tag)
         
 
 if __name__ == '__main__':
